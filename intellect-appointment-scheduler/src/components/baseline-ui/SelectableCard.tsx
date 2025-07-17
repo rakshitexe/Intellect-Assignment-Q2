c280247701085f6589d46d32d8379c416293e534
rakshitexe <rakshit.cse@gmail.com>
2025-07-18 01:32:40 +0530

    Fixed style bug

--- intellect-appointment-scheduler/src/components/baseline-ui/SelectableCard.tsx
+++ intellect-appointment-scheduler/src/components/baseline-ui/SelectableCard.tsx
@@ -17,7 +17,7 @@ const SelectableCard: React.FC<SelectableCardProps> = ({
   const baseStyle = `transition-all rounded-lg border text-[12px] text-center shadow-sm`;
 
   // Style for selected vs default states
-  const selectedStyle = `bg-[#eaeff3] text-black border-[#c8cdd1]`;
+  const selectedStyle = `bg-[#eaeff3] text-black border-[#c8cdd1] font-semibold`;
   const defaultStyle = `bg-white text-gray-800 border-[#d9dadb] hover:border-gray-400 hover:bg-gray-100`;
   // Combine final className
   const classes = `${baseStyle} ${isSelected ? selectedStyle : defaultStyle}`;
